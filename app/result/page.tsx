5039eac5f20abe4bc3668206bdd1e0baf759eb6d
KhromenkoDaniel <daniilkhro.work@gmail.com>
2025-01-22 01:52:13 +0200

    refactor: remove localstorage logic, optimize the routing

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,16 +1,27 @@
 'use client';
 
-import { useAppSelector } from '@/lib/hooks';
+import Link from 'next/link';
+
+import { clearSurvey } from '@/lib/features/survey/surveySlice';
+import { useAppSelector, useAppDispatch } from '@/lib/hooks';
 import styles from '@/styles/pages/Result.module.scss';
 
 export default function ResultPage() {
+  const dispatch = useAppDispatch();
   const responses = useAppSelector((state) => state.survey.responses);
+
+  const handleRestart = () => {
+    dispatch(clearSurvey());
+  };
 
   if (responses.length === 0) {
     return (
       <div className={styles.wrapper}>
         <h1 className={styles.title}>No responses found</h1>
         <p className={styles.subtitle}>Please complete the survey first.</p>
+        <Link href={'/'} className={styles.button} onClick={handleRestart}>
+          Restart
+        </Link>
       </div>
     );
   }
@@ -25,6 +36,9 @@ export default function ResultPage() {
           </li>
         ))}
       </ul>
+      <Link href={'/'} className={styles.button} onClick={handleRestart}>
+        Restart
+      </Link>
     </div>
   );
 }
